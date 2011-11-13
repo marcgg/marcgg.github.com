ff7030754b88f162779eda412a53d1790bb0f484
Marc G Gauthier <marcg.gauthier@gmail.com>
2011-11-14 00:02:06 +0100

    stop compiling maybe

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ Marcgg::Application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
   config.serve_static_assets = false
-  config.assets.compress = true
+  config.assets.compress = false
   config.assets.compile = false
   config.assets.digest = true
   config.i18n.fallbacks = true
