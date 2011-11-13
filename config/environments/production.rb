39241972267fe088178059c1cfcb430edef1ea88
Marc G Gauthier <marcg.gauthier@gmail.com>
2011-11-14 00:04:45 +0100

    compile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,9 +2,9 @@ Marcgg::Application.configure do
   config.cache_classes = true
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
+  config.assets.compile = true
   config.serve_static_assets = false
   config.assets.compress = false
-  config.assets.compile = false
   config.assets.digest = true
   config.i18n.fallbacks = true
   config.active_support.deprecation = :notify
