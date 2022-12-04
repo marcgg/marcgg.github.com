07c46fe02d88fac9466638e08c98d5055ff2e527
Marc G Gauthier <marcg.gauthier@gmail.com>
2022-12-04 22:07:31 +0100

    Use new comment system

--- assets/js/comments.js
+++ assets/js/comments.js
@@ -1,40 +1,55 @@
-$(document).ready(function(){
-  var url = "https://disqus.com/api/3.0/threads/list.json"
-  var api_key = "jo71Z19p42l7daP6JLOXUOBTPKvcyi1x1lo0DuGEugrRUXEFNaf3OKvaNS5byfzV"
-  var forum = "marcgg"
-  var thread = "link:" + window.location.href
-  $.ajax({
-    url: url,
-    dataType: "json",
-    data: {
-      api_key: api_key,
-      forum: forum,
-      thread: thread
-    },
-    success: function(data){
-      var $show = $("#show-disqus")
-      var postsCount = data.response[0].posts
-      var message = ""
-      if(postsCount == 0){
-        message += "Be the first to contribute with a comment"
-      }else if (postsCount == 1){
-        message += "Read the comments or contribute with your own"
-      }else{
-        message += "Read the " + postsCount + " comments or contribute with your own"
+$(document).ready(function() {
+  var baseUrl = "http://localhost:3000/" //"https://workwithswiftui.com/"
+
+  if($("#comment_form_wrapper").length > 0) {
+    $.ajax({
+      url: baseUrl + "comments/marcgg/",
+      type: "GET",
+      crossDomain: true,
+      data: { url: window.location.pathname },
+      dataType: "html",
+      success: function (response) {
+        $("#comments").html(response)
+      },
+      error: function (xhr, status) {
       }
-      $show.html(message)
-    },
-    statusCode: {
-      400: function() {},
-      500: function() {}
-    }
+    });
+  }
+
+  $("#comment_content").on("click", function(e) {
+    $("#comment_additional").show()
+  });
+
+  $("#comment_form").on("submit", function(e) {
+    e.preventDefault()
+    $.ajax({
+      url: baseUrl + "comments/marcgg/",
+      type: "POST",
+      crossDomain: true,
+      data: {
+        url: window.location.pathname,
+        comment_content: $("#comment_content").val(),
+        comment_name: $("#comment_name").val(),
+        comment_email: $("#comment_email").val(),
+        comment_website: $("#comment_website").val(),
+      },
+      beforeSend: function() {
+        $("#submit_comment").hide()
+        $("#submit_loading").show()
+      },
+      complete: function() {
+        $("#submit_comment").show()
+        $("#submit_loading").hide()
+      },
+      dataType: "html",
+      success: function (response) {
+        $("#comments").html(response)
+        $("input[type='text']").val("")
+        $("textarea").val("")
+      },
+      error: function (xhr, status) {
+      }
+    });
   })
 })
 
-document.getElementById('show-disqus').onclick=function(e){
-  e.preventDefault();
-  document.getElementById("disqus_hide").innerHTML = "";
-  document.getElementById("show-disqus").innerHTML = "";
-  document.getElementById("show-disqus-arrow").innerHTML = "";
-
-}
